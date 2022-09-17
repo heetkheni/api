8cea657a042ad1625f2ed3405ae0a857f2e32052
Heet Kheni <heetkheni47@gmail.com>
2022-09-17 13:06:54 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask,jsonify
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+place = [
+    {
+        'palace':"surat",
+    }
+]
 @app.route('/')
 def hello_world():
-    return "welcome"
+    return 'hello'
 
-if __name__==("__main__"):
+if __name__=="__main__":
     app.run(debug=True)
 
